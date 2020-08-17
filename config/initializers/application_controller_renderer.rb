ac75ab391772c6f15c164eab2461819023528604
Abeilles <afrojack.dj0628@gmail.com>
2020-08-18 00:26:54 +0900

    [update]3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '20c6e912ac080a5be1bdc66956b0ba524627863a2083dbd05d711a31002b87fd4dd9561baac1c9081095f78ba6122bf243b98e4bdd256a6ee4635c27b5b10e3f'
